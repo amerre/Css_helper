2895d035e7117ecefc64a382e5bf352fcaf80b2d
amerre <adr.merre@gmail.com>
2019-01-23 07:47:26 +0100

    :memo: Writing todos

--- src/components/Rules.vue
+++ src/components/Rules.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="rules">
+    <!--
+      TODO: Animer les entrées/sorties
+      TODO: Designer l'app correctement
+      TODO: Ajouter les deux dernieres regles
+      TODO: Upload le projet et le linker sur github
+    -->
     <app-Sizing></app-Sizing>
     <app-Color></app-Color>
     <app-transform></app-transform>
